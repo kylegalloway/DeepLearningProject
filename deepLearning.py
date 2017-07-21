64461d61d390c259b6fdcf64b6d937b433d6c46e
Kyle Galloway <kyle.galloway.1993@gmail.com>
2017-07-21 01:32:13 -0500

    Done Scraping

--- deepLearning.py
+++ deepLearning.py
@@ -1,6 +1,5 @@
-from imdbpie import Imdb
-from sklearn.cluster import SpectralCoclustering
 import itertools
+import json
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,96 +8,22 @@ import pickle
 import seaborn as sns
 import time
 import tmdbsimple as tmdb
-# import json
-# import random
-# import requests
-# import urllib2
-# import wget
+import urllib.request
+
+from imdbpie import Imdb
+from sklearn.cluster import SpectralCoclustering
 
 api_key = ''
 tmdb.API_KEY = api_key
-search = tmdb.Search()
-imbd_object = Imdb()
-imdb = Imdb(anonymize=True)  # to proxy requests
 
 # set the path where you want the scraped folders to be saved
 poster_folder = 'posters_final/'
 if poster_folder.split('/')[0] not in os.listdir('./'):
     os.mkdir('./' + poster_folder)
 
-
-def main():
-    # print("TMDB: ", get_movie_genres_tmdb("The Sandlot"))
-    # print("IMDB: ", get_movie_genres_imdb("The Sandlot"))
-
-    # Only use once, then use the pickle file afterwards
-    # pull_top_1000_movies_from_internet()
-
-    top1000_movies = load_top1000_movies_from_pickle()
-
-    allPairs = []
-    for movie in top1000_movies:
-        allPairs.extend(list2pairs(movie['genre_ids']))
-
-    num_ids = np.unique(allPairs)
-    visGrid = np.zeros((len(num_ids), len(num_ids)))
-    for p in allPairs:
-        visGrid[np.argwhere(num_ids == p[0]),
-                np.argwhere(num_ids == p[1])] += 1
-        if p[1] != p[0]:
-            visGrid[np.argwhere(num_ids == p[1]),
-                    np.argwhere(num_ids == p[0])] += 1
-
-    genres_dict = make_genre_dict()
-
-    model = SpectralCoclustering(n_clusters=5)
-    model.fit(visGrid)
-
-    fit_data = visGrid[np.argsort(model.row_labels_)]
-    fit_data = fit_data[:, np.argsort(model.column_labels_)]
-
-    annot_lookup_sorted = []
-    for i in np.argsort(model.row_labels_):
-        annot_lookup_sorted.append(genres_dict[num_ids[i]])
-
-    sns.heatmap(fit_data, xticklabels=annot_lookup_sorted,
-                yticklabels=annot_lookup_sorted, annot=False)
-    plt.title("After biclustering; rearranged to show biclusters")
-
-    plt.show()
-
-
-def pull_top_1000_movies_from_internet():
-    all_movies = tmdb.Movies()
-    top1000_movies = []
-    print('Pulling movie list, Please wait...')
-    for i in range(1, 51):
-        if i % 15 == 0:
-            time.sleep(7)
-        movies_on_this_page = all_movies.popular(page=i)['results']
-        top1000_movies.extend(movies_on_this_page)
-    len(top1000_movies)
-    f3 = open('movie_list.pckl', 'wb')
-    pickle.dump(top1000_movies, f3)
-    f3.close()
-    print('Done')
-
-
-def load_top1000_movies_from_pickle():
-    f3 = open('movie_list.pckl', 'rb')
-    top1000_movies = pickle.load(f3)
-    f3.close()
-    return top1000_movies
-
-
-# This function just generates all possible pairs of movies
-def list2pairs(l):
-    # itertools.combinations(l,2) makes all pairs of length 2 from list l.
-    pairs = list(itertools.combinations(l, 2))
-    # Get the one item pairs, duplicate pairs aren't accounted for by itertools
-    for i in l:
-        pairs.append([i, i])
-    return pairs
+search = tmdb.Search()
+imbd_object = Imdb()
+imdb = Imdb(anonymize=True)  # to proxy requests
 
 
 def make_genre_dict():
@@ -113,6 +38,206 @@ def make_genre_dict():
     return genre_dict
 
 
+Genre_ID_to_name = make_genre_dict()
+
+
+def main():
+    # print("TMDB: ", get_movie_genres_tmdb("The Sandlot"))
+    # print("IMDB: ", get_movie_genres_imdb("The Sandlot"))
+
+    # Only use once, then use the pickle file afterwards
+    # pull_top_1000_movies_from_internet()
+
+    top1000_movies = load_top1000_movies_from_pickle()
+
+    # make_and_show_heatmap(top1000_movies)
+    # cluster_data_and_show_heatmap(top1000_movies)
+
+    # Only use once, then use the pickle file afterwards
+    # pull_movies_for_all_unique_genre_pairs_from_internet()
+    movies = load_movies_for_all_unique_genre_pairs_from_pickle()
+    unique_movies = remove_duplicates(movies)
+
+    # Only use once, then use the pickle file afterwards
+    # pull_posters_for_movies_from_internet(unique_movies)
+    movies_with_poster = load_posters_for_movies_from_pickle(movies)[0]
+    movies_without_poster = load_posters_for_movies_from_pickle(movies)[1]
+
+
+def remove_duplicates(movies):
+    movie_ids = [m['id'] for m in movies]
+    movie_ids = np.unique(movie_ids)
+    seen_before = []
+    no_duplicate_movies = []
+    for i in range(len(movies)):
+        movie = movies[i]
+        id = movie['id']
+        if id not in seen_before:
+            seen_before.append(id)
+            no_duplicate_movies.append(movie)
+
+    return no_duplicate_movies
+
+
+def make_and_show_heatmap(movies_list):
+    allPairs = []
+    for movie in movies_list:
+        allPairs.extend(list2pairs(movie['genre_ids']))
+
+    num_ids = np.unique(allPairs)
+    visGrid = np.zeros((len(num_ids), len(num_ids)))
+    for p in allPairs:
+        visGrid[np.argwhere(num_ids == p[0]),
+                np.argwhere(num_ids == p[1])] += 1
+        if p[1] != p[0]:
+            visGrid[np.argwhere(num_ids == p[1]),
+                    np.argwhere(num_ids == p[0])] += 1
+
+    annot_lookup = []
+    for i in range(len(num_ids)):
+        annot_lookup.append(Genre_ID_to_name[num_ids[i]])
+
+    sns.heatmap(visGrid, xticklabels=annot_lookup, yticklabels=annot_lookup)
+    plt.title("Heatmap of pairings of genres")
+    plt.show()
+
+
+def cluster_data_and_show_heatmap(movies_list):
+    allPairs = []
+    for movie in movies_list:
+        allPairs.extend(list2pairs(movie['genre_ids']))
+
+    num_ids = np.unique(allPairs)
+    visGrid = np.zeros((len(num_ids), len(num_ids)))
+    for p in allPairs:
+        visGrid[np.argwhere(num_ids == p[0]),
+                np.argwhere(num_ids == p[1])] += 1
+        if p[1] != p[0]:
+            visGrid[np.argwhere(num_ids == p[1]),
+                    np.argwhere(num_ids == p[0])] += 1
+
+    model = SpectralCoclustering(n_clusters=5)
+    model.fit(visGrid)
+
+    fit_data = visGrid[np.argsort(model.row_labels_)]
+    fit_data = fit_data[:, np.argsort(model.column_labels_)]
+
+    annot_lookup_sorted = []
+    for i in np.argsort(model.row_labels_):
+        annot_lookup_sorted.append(Genre_ID_to_name[num_ids[i]])
+
+    sns.heatmap(fit_data, xticklabels=annot_lookup_sorted,
+                yticklabels=annot_lookup_sorted, annot=False)
+    plt.title("After biclustering; rearranged to show biclusters")
+    plt.show()
+
+
+def pull_top_1000_movies_from_internet():
+    all_movies = tmdb.Movies()
+    top1000_movies = []
+    print('Pulling movie list, Please wait...')
+    for i in range(1, 51):
+        if i % 15 == 0:
+            time.sleep(7)
+        movies_on_this_page = all_movies.popular(page=i)['results']
+        top1000_movies.extend(movies_on_this_page)
+    len(top1000_movies)
+    f = open('movie_list.pckl', 'wb')
+    pickle.dump(top1000_movies, f)
+    f.close()
+    print('Done')
+
+
+def load_top1000_movies_from_pickle():
+    f = open('movie_list.pckl', 'rb')
+    top1000_movies = pickle.load(f)
+    f.close()
+    return top1000_movies
+
+
+def pull_movies_for_all_unique_genre_pairs_from_internet():
+    allPairs = []
+    for movie in top1000_movies:
+        allPairs.extend(list2pairs(movie['genre_ids']))
+
+    num_ids = np.unique(allPairs)
+    movies = []
+    baseyear = 2017
+
+    done_ids = []
+    for g_id in num_ids:
+        baseyear -= 1
+        for page in range(1, 6):
+            time.sleep(0.5)
+
+            url = 'https://api.themoviedb.org/3/discover/movie?api_key='
+            url += api_key
+            url += '&language=en-US&sort_by=popularity.desc&year='
+            url += str(baseyear)
+            url += '&with_genres=' + str(g_id) + '&page=' + str(page)
+
+            print(url)
+            data = urllib.request.urlopen(url).read().decode('UTF-8')
+
+            dataDict = json.loads(data)
+            movies.extend(dataDict["results"])
+        done_ids.append(str(g_id))
+
+    f = open("movies_for_posters.pckl", 'wb')
+    pickle.dump(movies, f)
+    f.close()
+
+
+def load_movies_for_all_unique_genre_pairs_from_pickle():
+    f = open("movies_for_posters.pckl", 'rb')
+    movies = pickle.load(f)
+    f.close()
+    return movies
+
+
+def pull_posters_for_movies_from_internet(movies):
+    poster_movies = []
+    movies_no_poster = []
+    for movie in movies:
+        id = movie['id']
+        title = movie['title']
+        try:
+            grab_poster_tmdb(title)
+            poster_movies.append(movie)
+        except:
+            try:
+                time.sleep(7)
+                grab_poster_tmdb(title)
+                poster_movies.append(movie)
+            except:
+                movies_no_poster.append(movie)
+    f = open('poster_movies.pckl', 'w')
+    pickle.dump(poster_movies, f)
+    f.close()
+    f = open('no_poster_movies.pckl', 'w')
+    pickle.dump(movies_no_poster, f)
+    f.close()
+
+
+def load_posters_for_movies_from_pickle(movies):
+    f = open('poster_movies.pckl', 'r')
+    poster_movies = pickle.load(f)
+    f.close()
+    f = open('no_poster_movies.pckl', 'r')
+    movies_no_poster = pickle.load(f)
+    f.close()
+    return (poster_movies, movies_no_poster)
+
+
+def list2pairs(l):
+    # itertools.combinations(l,2) makes all pairs of length 2 from list l.
+    pairs = list(itertools.combinations(l, 2))
+    # Get the one item pairs, duplicate pairs aren't accounted for by itertools
+    for i in l:
+        pairs.append([i, i])
+    return pairs
+
+
 def grab_poster_tmdb(movie):
     response = search.movie(query=movie)
     id = response['results'][0]['id']
@@ -121,8 +246,8 @@ def grab_poster_tmdb(movie):
     title = movie.info()['original_title']
     url = 'image.tmdb.org/t/p/original' + poster_path
     title = '_'.join(title.split(' '))
-    command = 'wget -O ' + poster_folder + title + '.jpg ' + url
-    os.system(command)
+    output_file = poster_folder + title + '.jpg'
+    urllib.request.urlretrieve(url, filename=output_file)
 
 
 def get_movie_id_tmdb(movie):
@@ -155,13 +280,3 @@ def get_movie_genres_tmdb(movie):
 
 if __name__ == "__main__":
     main()
-
-
-# try:
-#     search.movie(query=movie) #An API request
-# except:
-#     try:
-#         time.sleep(10) #sleep for a bit, to give API requests a rest.
-#         search.movie(query=<i>movie_name</i>) #Make second API request
-#     except:
-#         print "Failed second attempt, check if there's an error in request"
